9b3244814e940a7db410649fe3e4d009d3e843d7
caxafa1830-pixel <caxafa1830@gmail.com>
2026-03-27 18:13:10 +0900

    ログアウト後のリダイレクト先をトップページに修正

--- app/controllers/logouts_controller.rb
+++ app/controllers/logouts_controller.rb
@@ -1,7 +1,7 @@
 class LogoutsController < ApplicationController
   def show
-    session.delete(:user_id)
-    redirect_to new_login_path, notice: "ログアウトしました"
+   session.delete(:user_id)
+   redirect_to root_path, notice: "ログアウトしました"
   end
 end
 
