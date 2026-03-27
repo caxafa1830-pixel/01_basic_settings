701f9aa84ed52867b4ca17a3422c2270bd3861fd
caxafa1830-pixel <caxafa1830@gmail.com>
2026-03-27 12:04:29 +0900

    電話番号と住所のカラムを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,8 +46,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def user_params
-    params.require(:user).permit(:name, :age, :tel)
+    def user_params
+      params.require(:user).permit(:name, :age, :tel, :address)  
   end
 
   def set_index_title
