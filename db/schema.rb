50e2112c517321fbd1a5ea502f033bd905c7fe31
caxafa1830-pixel <caxafa1830@gmail.com>
2026-03-28 10:52:15 +0900

    add columns title and description to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_03_27_052111) do
+ActiveRecord::Schema[7.0].define(version: 2026_03_28_012431) do
+  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.integer "age"
